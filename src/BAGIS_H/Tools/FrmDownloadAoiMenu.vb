b3206ea8f71e311aebb04bd471e76ee29a1239a2
Lesley <lbross@pdx.edu>
2015-03-20 15:29:02 -0700

    Migrate changes from svn lbross branch to git ebagis branch

---
+++ src/BAGIS_H/Tools/FrmDownloadAoiMenu.vb
@@ -0,0 +1,120 @@
+﻿Imports System.Windows.Forms
+Imports System.Net
+Imports System.Text
+Imports System.IO
+
+Public Class FrmDownloadAoiMenu
+
+    Private m_token As BagisToken = New BagisToken
+    'In practice the user name/password will be provided by the user
+    Private m_userName As String = Nothing
+    Private m_password As String = Nothing
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        'Set the user name and password from a text file that is NOT in source countrol
+        'Note: Developers will have to change this to a path valid on their machine
+        Dim filePath As String = "C:\Docs\Lesley\Repository\vb\BAGIS_H\branches\lbross\src\BAGIS_H\GoldenTicket.txt"
+        '@ToDo: In the future, this comes from user input
+        Try
+            ' Create an instance of StreamReader to read from a file.
+            ' The using statement also closes the StreamReader.
+            Using sr As New StreamReader(filePath)
+                m_userName = sr.ReadLine()
+                m_password = sr.ReadLine()
+            End Using
+        Catch e As Exception
+            ' Let the user know what went wrong.
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(e.Message)
+        End Try
+
+        ' Add any initialization after the InitializeComponent() call.
+        '---create a row---
+        Dim item As New DataGridViewRow
+        item.CreateCells(AoiGrid)
+        With item
+            .Cells(0).Value = "Price_R_at_Woodside_01232014"
+            .Cells(1).Value = "23-JAN-2014"
+            .Cells(2).Value = "G. Duh"
+            .Cells(3).Value = False
+            .Cells(4).Value = "Updated AOI with new gauge station"
+        End With
+        Dim item2 As New DataGridViewRow
+        item2.CreateCells(AoiGrid)
+        With item2
+            .Cells(0).Value = "Santa_Fe_R_nr_Santa_Fe_11302012"
+            .Cells(1).Value = "11-NOV-2012"
+            .Cells(2).Value = "D. Garen"
+            .Cells(3).Value = False
+            .Cells(4).Value = "Initial upload of AOI; Includes HRU definition"
+        End With
+        '---add the row---
+        '@ToDo: Temporarily stop populating form
+        'AoiGrid.Rows.Add(item)
+        'AoiGrid.Rows.Add(item2)
+        AoiGrid.ClearSelection()
+        AoiGrid.CurrentCell = Nothing
+
+        'Check for token
+        'm_token.token = SecurityHelper.GetStoredToken
+    End Sub
+
+    Private Sub BtnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnDownloadAoi_Click(sender As System.Object, e As System.EventArgs) Handles BtnDownloadAoi.Click
+        If String.IsNullOrEmpty(m_token.token) Then
+            Dim strToken As String = SecurityHelper.GetServerToken(m_userName, m_password, TxtBasinsDb.Text & "api-token-auth/")
+            m_token.token = strToken
+            If String.IsNullOrEmpty(strToken) Then
+                MessageBox.Show("Invalid user name or password. Failed to connect to database.", "Failed Connection", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+        End If
+
+        Dim reqT As HttpWebRequest
+        Dim resT As HttpWebResponse
+        'The end point for getting a token for the web service
+        reqT = WebRequest.Create(TxtBasinsDb.Text & "aoi/")
+        'This is a GET request
+        reqT.Method = "GET"
+
+        'Retrieve the token and format it for the header; Token comes from caller
+        Dim cred As String = String.Format("{0} {1}", "Token", m_token.token)
+        'Put token in header
+        reqT.Headers(HttpRequestHeader.Authorization) = cred
+
+        Try
+            resT = CType(reqT.GetResponse(), HttpWebResponse)
+            'Printing the response to the Console for testing
+            Using SReader As System.IO.StreamReader = New System.IO.StreamReader(resT.GetResponseStream)
+                Debug.Print(SReader.ReadToEnd())
+            End Using
+        Catch ex As WebException
+            Debug.Print("Exception: " & ex.Message)
+        End Try
+    End Sub
+
+    'Testing how to connect to an unsecured connection
+    Protected Sub TestConnection()
+        Dim reqT As HttpWebRequest
+        Dim resT As HttpWebResponse
+        reqT = WebRequest.Create("http://basins.geog.pdx.edu/bagis_test/test.html")
+        reqT.Method = "GET"
+        Try
+            'Dim dataStreamT As System.IO.Stream = reqT.GetRequestStream()
+            resT = CType(reqT.GetResponse(), HttpWebResponse)
+            Dim responseStream As New System.IO.StreamReader(resT.GetResponseStream(), Encoding.UTF8)
+            Dim result As String = responseStream.ReadToEnd()
+            Debug.Print(result)
+        Catch ex As WebException
+            MsgBox(ex.InnerException)
+        End Try
+    End Sub
+
+End Class
---
+++ src/BAGIS_H/Tools/FrmWebservices.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmWebservices
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtWebService = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.BtnSet = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cboFields = New System.Windows.Forms.ComboBox()
+        Me.BtnFields = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TxtWebService
+        '
+        Me.TxtWebService.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.TxtWebService.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtWebService.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TxtWebService.Location = New System.Drawing.Point(41, 12)
+        Me.TxtWebService.Name = "TxtWebService"
+        Me.TxtWebService.ReadOnly = True
+        Me.TxtWebService.Size = New System.Drawing.Size(459, 22)
+        Me.TxtWebService.TabIndex = 64
+        Me.TxtWebService.Text = "http://atlas.geog.pdx.edu/arcgis/rest/services/AWDB_ALL/AWDB_SNOTEL_ALL/FeatureSe" & _
+    "rver/0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(7, 15)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 16)
+        Me.Label5.TabIndex = 63
+        Me.Label5.Text = "Url:"
+        '
+        'BtnSet
+        '
+        Me.BtnSet.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSet.Location = New System.Drawing.Point(508, 8)
+        Me.BtnSet.Name = "BtnSet"
+        Me.BtnSet.Size = New System.Drawing.Size(108, 30)
+        Me.BtnSet.TabIndex = 65
+        Me.BtnSet.Text = "Set"
+        Me.BtnSet.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 67
+        Me.Label1.Text = "Fields:"
+        '
+        'cboFields
+        '
+        Me.cboFields.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboFields.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboFields.FormattingEnabled = True
+        Me.cboFields.Location = New System.Drawing.Point(61, 43)
+        Me.cboFields.Name = "cboFields"
+        Me.cboFields.Size = New System.Drawing.Size(135, 24)
+        Me.cboFields.TabIndex = 68
+        '
+        'BtnFields
+        '
+        Me.BtnFields.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnFields.Location = New System.Drawing.Point(207, 40)
+        Me.BtnFields.Name = "BtnFields"
+        Me.BtnFields.Size = New System.Drawing.Size(108, 30)
+        Me.BtnFields.TabIndex = 69
+        Me.BtnFields.Text = "Get Fields"
+        Me.BtnFields.UseVisualStyleBackColor = True
+        '
+        'FrmWebservices
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(682, 262)
+        Me.Controls.Add(Me.BtnFields)
+        Me.Controls.Add(Me.cboFields)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnSet)
+        Me.Controls.Add(Me.TxtWebService)
+        Me.Controls.Add(Me.Label5)
+        Me.Name = "FrmWebservices"
+        Me.ShowIcon = False
+        Me.Text = "Webservices testing"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TxtWebService As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents BtnSet As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cboFields As System.Windows.Forms.ComboBox
+    Friend WithEvents BtnFields As System.Windows.Forms.Button
+End Class
